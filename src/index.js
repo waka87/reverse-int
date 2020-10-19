c858e955a7bf64d1dbb9ede162419109da06dec6
waka87 <a.shaumenova87@gmail.com>
2020-10-20 00:18:17 +0600

    init: start task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  if (n > 0) {
+  var a = n.toString();
+  var arr = Array.from(a).reverse().map(Number).join('');
+  return arr;
+  } else {
+    n = n * (-1); 
+    var a = n.toString();
+  var arr = Array.from(a).reverse().map(Number).join('');
+  return arr;
+  }
+
 }
